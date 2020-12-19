d961e604a07ca471c5a9a5c3ea88a48c15dc803e
quintussangram <sangram.quintus@gmail.com>
2020-12-19 18:24:12 +0530

    login_redirect

--- apps/urls.py
+++ apps/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from apps.views import index, dashboard, logout
+from apps.views import index, dashboard, logout, login_redirect
 
 urlpatterns = [
     path('', index, name='index'),
     path('dashboard/', dashboard, name="dashboard"),
-    path('logout/', logout, name='logout')
+    path('logout/', logout, name='logout'),
+    path('login_redirect/',login_redirect,name="login_redirect")
 ]
